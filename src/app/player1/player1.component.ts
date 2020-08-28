a36b73c548644714b7ff95bc14166e0a8eb2ee6d
Mason Borchard <masonborchard@gmail.com>
2020-08-28 16:57:17 -0700

    Beginning, middle, and end items sort themselves automatically.

--- src/app/player1/player1.component.ts
+++ src/app/player1/player1.component.ts
@@ -1,7 +1,10 @@
-import { Component} from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { HttpClient } from '@angular/common/http';
-
+import { Component } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
+import { HttpClient } from "@angular/common/http";
 
 export interface Fragments {
   order: number;
@@ -10,42 +13,32 @@ export interface Fragments {
   end: string;
 }
 @Component({
-  selector: 'player1',
-  templateUrl: './player1.component.html',
-  styleUrls: ['./player1.component.scss']
+  selector: "player1",
+  templateUrl: "./player1.component.html",
+  styleUrls: ["./player1.component.scss"],
 })
 export class Player1Component {
-
   beginnings = [
-    'Beginning 1',
-    'Beginning 2',
-    'Beginning 3',
-    'Beginning 4',
-    'Beginning 5',
-    'Beginning 6'
+    "Beginning 1",
+    "Beginning 2",
+    "Beginning 3",
+    "Beginning 4",
+    "Beginning 5",
+    "Beginning 6",
   ];
 
   middles = [
-    'Middle 1',
-    'Middle 2',
-    'Middle 3',
-    'Middle 4',
-    'Middle 5',
-    'Middle 6'
+    "Middle 1",
+    "Middle 2",
+    "Middle 3",
+    "Middle 4",
+    "Middle 5",
+    "Middle 6",
   ];
 
-  ends = [
-    'End 1',
-    'End 2',
-    'End 3',
-    'End 4',
-    'End 5',
-    'End 6'
-  ];
+  ends = ["End 1", "End 2", "End 3", "End 4", "End 5", "End 6"];
 
-  response = [
-
-  ];
+  response = [];
 
   itemsWithOrder: any;
 
@@ -55,71 +48,97 @@ export class Player1Component {
 
   viewOrderRes() {
     this.response.map((item, index) => {
-      console.log('this.response', this.response)
-      item = { ...item, order: index};
-      var itemVal = Object.values(item).join('');
-      itemVal = itemVal.slice(0, itemVal.length - 1)
-      if (this.beginnings.indexOf(itemVal) !== -1){
+      console.log("this.response", this.response);
+      item = { ...item, order: index };
+      var itemVal = Object.values(item).join("");
+      itemVal = itemVal.slice(0, itemVal.length - 1);
+      if (this.beginnings.indexOf(itemVal) !== -1) {
         this.response[0] = item;
-      } else if (this.middles.indexOf(itemVal) !== -1){
+      } else if (this.middles.indexOf(itemVal) !== -1) {
         this.response[1] = item;
-      } else if (this.ends.indexOf(itemVal) !== -1){
+      } else if (this.ends.indexOf(itemVal) !== -1) {
         this.response[2] = item;
       }
-
     });
-    console.log('this.itemsWithOrder', this.itemsWithOrder )
+    console.log("this.itemsWithOrder", this.itemsWithOrder);
   }
 
   drop(event: CdkDragDrop<string[]>) {
+    if (
+      event.previousContainer.id === "beginning-options" &&
+      event.container.id === "response-submission"
+    ) {
+      console.log(
+        "this.beginnings, event.previousContainer.data",
+        this.beginnings,
+        event.previousContainer.data,
+      );
 
-    if ((event.previousContainer.id === "beginning-options") && (event.container.id === "response-submission")) {
-      console.log('this.beginnings, event.previousContainer.data', this.beginnings, event.previousContainer.data
-      )
+      if (event.previousContainer.data.length < 6) {
+        if (event.currentIndex !== 0) {
+          event.currentIndex = 0;
+        }
 
-    if(event.previousContainer.data.length < 6){
-      if (event.currentIndex !== 0) {
-        event.currentIndex = 0;
+        transferArrayItem(
+          event.container.data,
+          event.previousContainer.data,
+          0,
+          event.previousContainer.data.length,
+        );
       }
 
-      transferArrayItem(event.container.data, event.previousContainer.data,
-        0, event.previousContainer.data.length);
-
-    }
-
-    transferArrayItem(event.previousContainer.data, event.container.data,
-      event.previousIndex, 0);
-
-    } else if ((event.previousContainer.id === "middle-options") && (event.container.id === "response-submission")) {
-
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        0,
+      );
+    } else if (
+      event.previousContainer.id === "middle-options" &&
+      event.container.id === "response-submission"
+    ) {
       if (event.currentIndex !== 1) {
         event.currentIndex = 1;
-
       }
 
-
-      if(event.previousContainer.data.length < 6){
-        transferArrayItem(event.container.data, event.previousContainer.data,
-          1, event.previousContainer.data.length);
+      if (event.previousContainer.data.length < 6) {
+        transferArrayItem(
+          event.container.data,
+          event.previousContainer.data,
+          1,
+          event.previousContainer.data.length,
+        );
       }
 
-      transferArrayItem(event.previousContainer.data, event.container.data,
-        event.previousIndex, 1)
-
-
-    } else if ((event.previousContainer.id === "end-options") && (event.container.id === "response-submission")) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        1,
+      );
+    } else if (
+      event.previousContainer.id === "end-options" &&
+      event.container.id === "response-submission"
+    ) {
       if (event.currentIndex !== 2) {
         event.currentIndex = 2;
 
-      if(event.previousContainer.data.length < 6){
-        transferArrayItem(event.container.data, event.previousContainer.data,
-          2, event.previousContainer.data.length);
-      }
-      transferArrayItem(event.previousContainer.data, event.container.data,
-        event.previousIndex, 2);
+        if (event.previousContainer.data.length < 6) {
+          transferArrayItem(
+            event.container.data,
+            event.previousContainer.data,
+            2,
+            event.previousContainer.data.length,
+          );
+        }
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          2,
+        );
         this.viewOrderRes();
-
+      }
     }
   }
-}
 }
